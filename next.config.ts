6d5b46c12d539c3ffd859f986dc44bf0965ba903
bunchofletters <97305426+bunchofletters@users.noreply.github.com>
2025-10-30 18:06:08 -0400

    test5

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
-  output: 'export',
-  basePath: '/kit_paper',
-  images: {
-    unoptimized: true
-  }
 };
 
 export default nextConfig;
